9a523f62b80c837872bc14ccfa070dc8e37a9b65
Andrew Dunstan <a_dunstan@hotmail.com>
2025-10-31 06:38:48 +1100

    fix some typos and add wiring comment

--- flexio_i2c_spy.ino
+++ flexio_i2c_spy.ino
@@ -1,6 +1,12 @@
 #include <Wire.h>
 #include "flexio_i2c_sniff.h"
 
+/* For Teensy 4.1: Connect the three I2C SCL pins together (pins 16, 19 and 24)
+ * and the three I2C SDA pins together (pins 17, 18 and 25).
+ * The I2C0/Wire port acts as a slave, I2C2/Wire2 acts and master and I2C1/Wire1 is
+ * routed to FlexIO3 to act as a sniffer.
+ */
+
 static uint8_t eeprom_addr;
 static uint8_t eeprom_data[4] = {0xAA,0xBB,0xCC,0xDD};
 
@@ -57,7 +63,7 @@ void loop() {
   int r;
   switch (Serial.read()) {
     case 'w':
-      // use: "w {X, Y, Z...}"" to write consecutive values
+      // use: "w {X, Y, Z...}" to write consecutive values
       Wire2.beginTransmission(0x50);
       while ((r = parseInt(Serial)) >= 0) {
         Wire2.write(r);
@@ -72,7 +78,7 @@ void loop() {
       // use: "r X Y" to read X bytes from address Y
       r = parseInt(Serial);
       Serial.print("requestFrom(0x50, ");
-      if (r < 0) {
+      if (r < 1) {
         r = Wire2.requestFrom(0x50, 1);
         Serial.print("1) returned ");
       }
